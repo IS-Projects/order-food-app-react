a9355c9bcf7884f4fe61beedb280f8ce82a38653
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-01-28 01:26:53 -0700

    fixed badge number of items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -5,13 +5,16 @@ import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   const ctx = useContext(CartContext);
+  const numberOfCartItems = ctx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>{ctx.items.length}</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
