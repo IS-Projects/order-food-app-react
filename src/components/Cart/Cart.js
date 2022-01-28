c8dc1bd3ed35037931d7c11234fc44e8b1e7b65b
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-01-27 22:27:02 -0700

    added classes from module css file

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,22 @@
+import classes from "./Cart.module.css";
 import CartItem from "./CartItem";
 
 const Cart = ({ mealsInCart }) => {
   return (
     <div>
-      <ul>
+      <ul className={classes["cart-items"]}>
         {mealsInCart.map((meal) => {
           return <CartItem />;
         })}
       </ul>
-      <div>
+      <div className={classes.total}>
+        {/* the video uses span for both things below */}
         <h3>Total Amount</h3>
         <p>88.99</p>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
       </div>
     </div>
   );
