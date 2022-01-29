ac8b6f39cc313750d2b327de947545b13635b91f
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-01-29 00:14:53 -0700

    added useEffect for cart animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   const ctx = useContext(CartContext);
-  const numberOfCartItems = ctx.items.reduce((curNumber, item) => {
+  const [buttonAnimation, setButtonAnimation] = useState(false);
+  const { items } = ctx;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${buttonAnimation ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonAnimation(true);
+    const timer = setTimeout(() => {
+      setButtonAnimation(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
